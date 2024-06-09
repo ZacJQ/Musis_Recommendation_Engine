b8d30325fa24cc5ba7d1903c2ee214514a86a38f
Zaccaria Quadros <zac@Zaccarias-MacBook-Pro.local>
2024-06-10 00:24:52 +0530

    Classifier and Recommender added

--- models/classifier/classifier.py
+++ models/classifier/classifier.py
@@ -1,40 +1,35 @@
 import pandas as pd
+import os
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import classification_report
+from joblib import dump, load  # Added import for joblib
 
-# Load dataset
-dataset = pd.read_csv("/Users/zac/Codes/Music_Project/GIT_HUB/Musis_Recommendation_Engine/data/testing/testing.csv")
+base_dir = "/Users/zac/Codes/Music_Project/GIT_HUB/Musis_Recommendation_Engine"
+file_path = "exploration/Data_collection/final_filtered_mood_list.csv"
+
+dataset = pd.read_csv(os.path.join(base_dir,file_path))
+
+try:
+    dataset = dataset.rename(columns={"MOOD":"mood"})
+    dataset.drop(columns=["Unnamed: 0"], inplace=True)
+except Exception as e:
+    print("Already column removed", e)
 
 # Select relevant features
-features = ['valence', 'popularity', 'year', 'genre', 'danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'tempo', 'duration_ms']
+features = ['valence', 'popularity', 'year', 'danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'tempo', 'duration_ms']
 
 # Remove rows with missing values
-dataset = dataset.dropna(subset=features+['mood'])
+dataset = dataset.dropna(subset=features)
+dataset = dataset.drop(columns=['artist_name','track_name'])
 
-dataset = dataset.rename(columns={"MOOD":"mood"})
-dataset.drop(columns=["Unnamed: 0"], inplace=True)
-
-# # Encode categorical variables if any
-# dataset = pd.get_dummies(dataset, columns=['genre'])
-
-# Split dataset into features and target
+# Select features again after one-hot encoding
+features = dataset.columns.tolist()
 X = dataset[features]
-y = dataset['mood']
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X)
 
-# Split dataset into train and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+print(scaler.mean_ , scaler.scale_)
 
-# Initialize Random Forest Classifier
-classifier = RandomForestClassifier(n_estimators=100, random_state=42)
-
-# Train classifier
-classifier.fit(X_train, y_train)
-
-# Predict on test set
-y_pred = classifier.predict(X_test)
-
-# Evaluate classifier
-accuracy = accuracy_score(y_test, y_pred)
-print("Accuracy:", accuracy)
-print(classification_report(y_test, y_pred))
+loaded_model = load('model.joblib')
